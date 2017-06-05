0f8bb2978ed4a26d57594bcb3be5175254b5cb78
Challa <challh@amazon.com>
2017-06-05 13:55:06 +0530

    Addind code for reversing Linked List from m to n positions provided 1<=m<=n<=length of list

--- src/main/java/com/coding/practice/linkedlist/ReverseLinkedList.java
+++ src/main/java/com/coding/practice/linkedlist/ReverseLinkedList.java
@@ -5,17 +5,49 @@ public class ReverseLinkedList {
 	// return the new Root Node
 	public LinkedList process(LinkedList list) {
 		LLNode root = list.getRoot();
-		if(root == null)
+		if (root == null)
 			return list;
 		LLNode prev = null;
 		LLNode current = root;
-		
-		while(current != null) {
+
+		while (current != null) {
 			LLNode next = current.getNext();
 			current.setNext(prev);
 			prev = current;
 			current = next;
 		}
 		return new LinkedList(prev);
+	}
+
+	// return the new Root Node by reversing the list between start and end nodes
+	public LinkedList process(LinkedList list, int start, int end) {
+		LLNode root = list.getRoot();
+		if (root == null)
+			return list;
+		// Traverse through m nodes
+		int count = 1;
+		LLNode temp = root;
+		LLNode nodeBeforeStart = null;
+		while(count < start) {
+			nodeBeforeStart = temp;
+			temp = temp.getNext();
+			count++;
+		}
+		LLNode prev = null;
+		LLNode current = temp;
+		
+		while (count <= end) {
+			LLNode next = current.getNext();
+			current.setNext(prev);
+			prev = current;
+			current = next;
+			count++;
+		}
+		if(nodeBeforeStart != null) 
+			nodeBeforeStart.setNext(prev);
+		else
+			root = prev;
+		temp.setNext(current);
+		return new LinkedList(root);
 	}
 }
